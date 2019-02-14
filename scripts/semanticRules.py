8407376a8aadafb41a460c79fa8847d0ddaf5398
Tina Quach <quacht@mit.edu>
2019-02-14 18:39:39 -0500

    add rules for parsing multiword sound names

--- scripts/semanticRules.py
+++ scripts/semanticRules.py
@@ -497,7 +497,10 @@ sem.add_rule("NAME_OF_SOUND -> Det NAME_OF_SOUND", lambda d, name: name.title())
 sem.add_rule("NAME_OF_SOUND -> Your NAME_OF_SOUND", lambda d, name: name.title())
 sem.add_rule("NAME_OF_SOUND -> NAME_OF_SOUND Sound", lambda name, sound: name.title())
 sem.add_rule("NAME_OF_SOUND -> Sound Called NAME_OF_SOUND", lambda s, c, name: name.title())
-sem.add_rule("NAME_OF_SOUND -> Random Sound", lambda r, s: getRandomSound());
+sem.add_rule("NAME_OF_SOUND -> Random Sound", lambda r, s: getRandomSound())
+sem.add_rule("NAME_OF_SOUND -> Unk", lambda unk1: unk1)
+sem.add_rule("NAME_OF_SOUND -> Unk Unk", lambda unk1, unk2: unk1 + ' ' + unk2)
+sem.add_rule("NAME_OF_SOUND -> Unk Unk Unk", lambda unk1, unk2, unk3: unk1 + ' ' + unk2 + ' ' + unk3)
 
 sem.add_rule("SoundCommand -> Set TheVolume To NP", lambda sett, volume, too, unk: singleCommand("setVolumeTo:", unk))
 sem.add_rule("SoundCommand -> Set TheVolume To NP Percent", lambda sett, volume, too, unk, percent: singleCommand("setVolumeTo:", unk))
