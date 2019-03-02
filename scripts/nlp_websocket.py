8369513837fe87a7f045f377a09ec9a132f29dc2
Tina Quach <quacht@mit.edu>
2019-03-02 13:08:40 -0500

    delete extra code

--- scripts/nlp_websocket.py
+++ scripts/nlp_websocket.py
@@ -1,45 +1,5 @@
 #!/usr/bin/env python
 
-# import websocket
-
-# try:
-#     import thread
-# except ImportError:
-#     import _thread as thread
-# import time
-
-# def on_message(ws, message):
-#     print(message)
-
-# def on_error(ws, error):
-#     print(error)
-
-# def on_close(ws):
-#     print("### closed ###")
-
-# def on_open(ws):
-#     def run(*args):
-#         for i in range(3):
-#             time.sleep(1)
-#             ws.send("Hello %d" % i)
-#         time.sleep(1)
-#         ws.close()
-#         print("thread terminating...")
-#     thread.start_new_thread(run, ())
-
-
-# if __name__ == "__main__":
-#     websocket.enableTrace(True)
-#     ws = websocket.WebSocketApp("ws://localhost:8765",
-#                               on_message = on_message,
-#                               on_error = on_error,
-#                               on_close = on_close)
-#     ws.on_open = on_open
-#     ws.run_forever()
-
-
-###################################
-
 from websocket_server import WebsocketServer
 import json
 
@@ -56,8 +16,6 @@ def client_left(client, server):
 
 # Called when a client sends a message
 def message_received(client, server, message):
-	if len(message) > 200:
-		message = message[:200]+'..'
 	print("Client(%d) said: %s" % (client['id'], message))
 	messageObject = json.loads(message)
 	server.send_message_to_all(json.dumps({"id":messageObject["id"], "response":"echo echo"}));
