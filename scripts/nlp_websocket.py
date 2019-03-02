57d858d71fe0a2a99d16e338c87052d91b523ec6
Tina Quach <quacht@mit.edu>
2019-03-02 11:57:32 -0500

    messaging works between vui and nlp

---
+++ scripts/nlp_websocket.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# import websocket
+
+# try:
+#     import thread
+# except ImportError:
+#     import _thread as thread
+# import time
+
+# def on_message(ws, message):
+#     print(message)
+
+# def on_error(ws, error):
+#     print(error)
+
+# def on_close(ws):
+#     print("### closed ###")
+
+# def on_open(ws):
+#     def run(*args):
+#         for i in range(3):
+#             time.sleep(1)
+#             ws.send("Hello %d" % i)
+#         time.sleep(1)
+#         ws.close()
+#         print("thread terminating...")
+#     thread.start_new_thread(run, ())
+
+
+# if __name__ == "__main__":
+#     websocket.enableTrace(True)
+#     ws = websocket.WebSocketApp("ws://localhost:8765",
+#                               on_message = on_message,
+#                               on_error = on_error,
+#                               on_close = on_close)
+#     ws.on_open = on_open
+#     ws.run_forever()
+
+
+###################################
+
+from websocket_server import WebsocketServer
+
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+	print("New client connected and was given id %d" % client['id'])
+	server.send_message_to_all("Hey all, a new client has joined us")
+
+
+# Called for every client disconnecting
+def client_left(client, server):
+	print("Client(%d) disconnected" % client['id'])
+
+
+# Called when a client sends a message
+def message_received(client, server, message):
+	if len(message) > 200:
+		message = message[:200]+'..'
+	print("Client(%d) said: %s" % (client['id'], message))
+
+
+PORT=8765
+server = WebsocketServer(PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
