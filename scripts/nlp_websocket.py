197928593b4478d58615775bd5e1d320615026d7
Tina Quach <quacht@mit.edu>
2019-03-02 12:56:28 -0500

    websockets as promised

--- scripts/nlp_websocket.py
+++ scripts/nlp_websocket.py
@@ -41,11 +41,12 @@
 ###################################
 
 from websocket_server import WebsocketServer
+import json
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+	server.send_message_to_all(json.dumps({"message":"Hey all, a new client has joined us"}))
 
 
 # Called for every client disconnecting
@@ -58,7 +59,8 @@ def message_received(client, server, message):
 	if len(message) > 200:
 		message = message[:200]+'..'
 	print("Client(%d) said: %s" % (client['id'], message))
-
+	messageObject = json.loads(message)
+	server.send_message_to_all(json.dumps({"id":messageObject["id"], "response":"echo echo"}));
 
 PORT=8765
 server = WebsocketServer(PORT)
